ba3382f72143c131a38f62a49665d000ab3bc3a3
Tanin <ber_zebub@hotmail.com>
2020-07-16 19:59:51 +0700

    Create Quasar 2.0.5

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn @click="test()"></q-btn>
+  </q-page>
+</template>
+
+<script>
+import { db } from "../router/index";
+export default {
+  name: "PageIndex",
+  data() {
+    return {};
+  },
+  methods: {
+    test() {
+      db.collection("test").add({ test: 1234 });
+    }
+  }
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import routes from "./routes";
+
+Vue.use(VueRouter);
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
+
+var firebaseConfig = {
+  apiKey: "AIzaSyArJ0zxkVN2dJnXE5wq2zCyvNfy_nwpoXk",
+  authDomain: "atwork-dee11.firebaseapp.com",
+  databaseURL: "https://atwork-dee11.firebaseio.com",
+  projectId: "atwork-dee11",
+  storageBucket: "atwork-dee11.appspot.com",
+  messagingSenderId: "617447385183",
+  appId: "1:617447385183:web:f9c8c29f85827b952153ea",
+  measurementId: "G-GJTSD57TQH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
+console.log(db);
+
+if (location.hostname === "localhost") {
+  db.settings({ host: "localhost:4005", ssl: false });
+}
+const storage = firebase.storage();
+export const st = storage.ref();
+export const auth = firebase.auth();
+// export const axios = require("axios").default;
+
+export default function(/* { store, ssrContext } */) {
+  const Router = new VueRouter({
+    scrollBehavior: () => ({ x: 0, y: 0 }),
+    routes,
+
+    // Leave these as they are and change in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    mode: process.env.VUE_ROUTER_MODE,
+    base: process.env.VUE_ROUTER_BASE
+  });
+
+  return Router;
+}
