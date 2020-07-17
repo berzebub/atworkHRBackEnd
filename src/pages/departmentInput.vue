97e6f0549b54cae75ab42509dbb3b5fdb542e440
kong <acornversion@gmail.com>
2020-07-17 16:54:53 +0700

    update departmentinput

--- src/pages/departmentInput.vue
+++ src/pages/departmentInput.vue
@@ -1,19 +1,25 @@
 <template>
   <q-page class="container">
     <div align="center">
-      <div class="q-pa-md text-left" style="max-width:328px">
+      <div class="q-pa-md text-left text-subtitle1" style="max-width:328px">
         <div>
           <span>ชื่อ นามสกุล</span>
-          <q-input dense outlined v-model="department.name" :rules="[val => !!val]" />
+          <q-input ref="name" dense outlined v-model="department.name" :rules="[val => !!val]" />
         </div>
         <div>
           <span>อีเมล</span>
-          <q-input dense outlined v-model="department.gmail" :rules="[val => !!val]" />
+          <q-input ref="gmail" dense outlined v-model="department.gmail" :rules="[val => !!val]" />
         </div>
         <div>
           <span>รหัสผ่าน</span>
-          <div class="text-grey-6">ไม่ต่ำกว่า 6 ตัวอักษร</div>
-          <q-input dense outlined v-model="department.password" :rules="[val => !!val]" />
+          <div class="text-grey-6 text-body2">ไม่ต่ำกว่า 6 ตัวอักษร</div>
+          <q-input
+            ref="password"
+            dense
+            outlined
+            v-model="department.password"
+            :rules="[val => !!val]"
+          />
         </div>
         <div class="row">
           <div class="self-center">สิทธิ์การใช้งาน</div>
@@ -30,14 +36,15 @@
           />
         </div>
         <div class="text-center q-gutter-lg q-pt-md">
-          <q-btn outline color="cyan-8" class="boxSave" label="ยกเลิก" />
-          <q-btn color="cyan-8" class="boxSave" label="บันทึก" />
+          <q-btn @click="cancelBtn()" outline color="cyan-8" class="boxSave" label="ยกเลิก" />
+          <q-btn @click="saveBtn()" color="cyan-8" class="boxSave" label="บันทึก" />
         </div>
       </div>
     </div>
   </q-page>
 </template>
 <script>
+// import { db, auth } from "../router/index";
 export default {
   data() {
     return {
@@ -75,6 +82,26 @@ export default {
           this.sanctionGroup = [];
         });
       }
+    },
+    cancelBtn() {
+      this.$router.push("/departmentMain");
+    },
+    saveBtn() {
+      this.$refs.name.validate();
+      this.$refs.gmail.validate();
+      this.$refs.password.validate();
+      if (
+        this.$refs.name.hasError ||
+        this.$refs.gmail.hasError ||
+        this.$refs.password.hasError
+      ) {
+      }
+      if (this.sanctionGroup == []) {
+        console.log("55555");
+      }
+      return;
+      // this.department.sanctionGroup = this.sanctionGroup;
+      // db.collection("user_admin").add(this.department);
     }
   }
 };
